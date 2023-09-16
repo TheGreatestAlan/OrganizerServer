112acab949bf6f45f53f071fc852649e581eff96
thegreatestalan <alan.hoang.nguyen@gmail.com>
2023-09-16 09:58:58 -0600

    adding environment variable configuration to dropwizard app

--- organizerserver/src/main/java/com/tippytappytoes/OrganizerServerApplication.java
+++ organizerserver/src/main/java/com/tippytappytoes/OrganizerServerApplication.java
@@ -4,7 +4,10 @@ import com.tippytappytoes.file.FileOrganizerRepo;
 import com.tippytappytoes.resources.InventoryResource;
 import com.tippytappytoes.services.Inventory;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 
 public class OrganizerServerApplication extends Application<OrganizerServerConfiguration> {
 
@@ -13,10 +16,21 @@ public class OrganizerServerApplication extends Application<OrganizerServerConfi
   }
 
   @Override
+  public void initialize(final Bootstrap<OrganizerServerConfiguration> bootstrap) {
+    // Enable environment variable substitution with a prefix of 'dw'
+    boolean strict = false; // set to true to throw exception if variable is not defined
+    bootstrap.setConfigurationSourceProvider(
+            new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                    new EnvironmentVariableSubstitutor(strict))
+    );
+  }
+
+  @Override
   public void run(final OrganizerServerConfiguration configuration,
-      final Environment environment) {
+                  final Environment environment) {
+
     FileOrganizerRepo fileOrganizerRepo = new FileOrganizerRepo(
-        configuration.getObsidianVaultRepoLocation());
+            configuration.getObsidianVaultRepoLocation());
     fileOrganizerRepo.getOrganizerInventory();
     Inventory inventory = new Inventory(fileOrganizerRepo);
     final InventoryResource inventoryResource = new InventoryResource(inventory);
