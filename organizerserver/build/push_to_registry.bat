2effb3bdfb2f599682f9c4070f5acc8213ccacf1
thegreatestalan <alan.hoang.nguyen@gmail.com>
2023-09-16 11:02:37 -0600

    registry push updates

--- organizerserver/build/push_to_registry.bat
+++ organizerserver/build/push_to_registry.bat
@@ -2,7 +2,9 @@
 
 REM Set the image name and tag you want to push
 SET IMAGE_NAME=organizerserver:latest
-SET LOCAL_REGISTRY=localhost:5000
+
+REM Check if LOCAL_REGISTRY environment variable is set, if not, default to localhost:5000
+IF "%LOCAL_REGISTRY%"=="" SET LOCAL_REGISTRY=localhost:5000
 
 REM Tag the image for the local registry
 docker tag %IMAGE_NAME% %LOCAL_REGISTRY%/%IMAGE_NAME%
